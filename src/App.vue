902b9967b6e47cbe5e3b1685aed6719c9008b246
student10 <student10@pcoip.ki.agh.edu.pl>
2019-04-27 11:31:57 +0200

    login work

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Witaj w systemie do zapisów na zajêcia</h1>
+    <!-- <p>{{ email }}</p> -->
+    
+    <div v-if="isAuthenticated">
+      Zalogowany jako {{ email }}
+      <a @click="logOut()"> Wyloguj</a>
+    </div>
+    <div v-else="isAuthenticated">
+      <label>Zaloguj siê e-mailem</label>
+      <input type="email" v-model="email">
+	  <button @click="logIn()">Wchodzê</button>
+    </div>
+    
+    <!-- 
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+	<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+	<div v-else>Twój adres e-mail jest stanowczo za d³ugi.</div>
+	 -->
+	 
   </div>
 </template>
 
@@ -9,10 +27,24 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	data() {
+		  return {
+		    email: '',
+		    isAuthenticated:false
+		  };
+	},
+	methods: {
+		  alertMyEmail() {
+		    alert(this.email);
+		  },
+		  logIn() {
+			  this.isAuthenticated = true;
+		  },
+		  logOut() {
+			  this.isAuthenticated = false;
+			  this.email = '';
+		  }
+	}
 }
 </script>
 
